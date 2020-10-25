3ac0a2545cbd97d8017825c3856fd42fb2bea2d5
Brice Michallski <brice_michalski@carrefour.com>
2020-10-25 18:29:41 +0100

    add redis & counter

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,9 +1,23 @@
 from flask import Flask
+from flask import Response
+from redis import StrictRedis
+
 app = Flask(__name__)
+bdd = StrictRedis(host="redis", port=6379, db=0, decode_responses=True)
 
-@app.route('/')
+
+@app.route("/")
 def hello_world():
-    return 'Hello World!'
+    counter = int(bdd.get("counter"))
+    counter += 1
+    bdd.set("counter", counter)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=False)
+    resp = Response("Hello World! {} visites".format(counter))
+
+    return resp
+
+
+if __name__ == "__main__":
+    if bdd.get("counter") == None:
+        bdd.set("counter", 0)
+    app.run(host="0.0.0.0", port=8080, debug=False)
